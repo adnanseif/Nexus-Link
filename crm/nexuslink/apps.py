c31b24571e80db8c2911d7c6c2af64c2aa63bf37
Adnan <adnanseif100@gmail.com>
2025-05-08 14:54:22 -0600

    Initial commit

---
+++ crm/nexuslink/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NexuslinkConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'nexuslink'
---
+++ crm/static/js/app.js
@@ -0,0 +1,12 @@
+// Message/Notification Timer
+
+var message_timeout = document.getElementById("message-timer");
+
+setTimeout(function()
+
+{
+
+    message_timeout.style.display = "none";
+
+
+}, 3000);
